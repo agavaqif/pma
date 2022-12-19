7943731988c684c0d1a83517ef7978b6425d9a35
Adrian Developer <adrian333dev@gmail.com>
2022-12-19 21:11:18 +0400

    Batch Update added in core

--- core/src/resources/kp/kp.service.ts
+++ core/src/resources/kp/kp.service.ts
@@ -1,11 +1,13 @@
-import { ExecType } from 'src/resources/exec-type/entities/exec-type.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import { ExecType } from 'src/resources/exec-type/entities/exec-type.entity';
 import { Project } from '../project/entities/project.entity';
 import { CreateKpsDto } from './dto/create-kps.dto';
 import { UpdateKpDto } from './dto/update-kp.dto';
 import { Kp } from './entities/kp.entity';
+import { BatchUpdateKpsDto } from './dto/batch-update-kps.dto';
 
 @Injectable()
 export class KpService {
@@ -47,6 +49,30 @@ export class KpService {
   async update(kpId: number, updateKpDto: UpdateKpDto) {
     const kp = this.repo.update(kpId, updateKpDto);
     return kp;
+  }
+
+  async updateBatch(projectId: number, batchUpdateKpsDto: BatchUpdateKpsDto) {
+    const { execTypeId, ranges } = batchUpdateKpsDto;
+    const kps = [];
+    for (const range of ranges) {
+      const { start, end } = range;
+      const kpsInRanges = await this.repo
+        .createQueryBuilder('kp')
+        .leftJoinAndSelect('kp.execType', 'execType')
+        .leftJoinAndSelect('kp.project', 'project')
+        .where('kp.start >= :start', { start })
+        .andWhere('kp.end <= :end', { end })
+        .andWhere({ project: { projectId } })
+        .getMany();
+      kps.push(...kpsInRanges);
+    }
+    console.log({
+      execTypeId,
+      kps,
+      projectId,
+    });
+    const updatedKps = await this.repo.update(kps, { execType: { execTypeId } as ExecType });
+    return updatedKps;
   }
 
   async remove(kpId: number) {
