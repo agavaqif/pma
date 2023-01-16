116750c4eb7f6420b8fbf95883a68e5abe7c64d5
Vagif <agavaqif@yahoo.com>
2023-01-16 15:44:08 -0600

    API Url update

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   mediaBreak: 768,
   isMobile: window.innerWidth < 768,
-  coreBaseUrl: 'http://3.21.100.216:3000',
+  coreBaseUrl: 'http://3.140.186.230:3000',
   pageSize: 250,
 };
